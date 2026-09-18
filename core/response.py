5d483039e90beda0e9381d9c8d7ba4222fa0883c
souad <souadquzmar13@gmail.com>
2026-09-18 22:23:41 +0300

    add custom response

---
+++ core/response.py
@@ -0,0 +1,12 @@
+from rest_framework.response import Response
+
+
+def success_response(*, data=None, message=None, status_code=200):
+    return Response(
+        {
+            "success": True,
+            "message": message,
+            "data": data,
+        },
+        status=status_code,
+    )
